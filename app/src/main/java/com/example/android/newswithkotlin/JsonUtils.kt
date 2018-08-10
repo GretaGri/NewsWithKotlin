64e484ad65b2a540dfcff68351586382a694e786
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2018-08-10 21:19:38 +0530

    Added Klaxon for Json parsing but issue with method `klaxon.parse()`

--- app/src/main/java/com/example/android/newswithkotlin/JsonUtils.kt
+++ app/src/main/java/com/example/android/newswithkotlin/JsonUtils.kt
@@ -1,15 +1,14 @@
 package com.example.android.newswithkotlin
 
-import org.json.JSONException
-import org.json.JSONObject
+import android.util.Log
+import com.beust.klaxon.JsonReader
+import com.beust.klaxon.Klaxon
+import java.io.StringReader
 
 /**
  * Created by Greta Grigutė on 2018-08-09.
  */
 class JsonUtils {
-    private lateinit var webTitle: String
-    private lateinit var newsWebUrl: String
-    private lateinit var authorTitle: String
 
     /**
      * Create a private constructor because no one should ever create a [JsonUtils] object.
@@ -18,61 +17,85 @@ class JsonUtils {
      */
     private fun JsonUtils() {}
 
-    /**
-     * Return a list of [News] objects that has been built up from
-     * parsing a JSON response.
-     */
-   fun extractFeatureFromJson(jsonResponse: String?): ArrayList<News> {
 
-        // Create an empty ArrayList that we can start adding news to
-        val news = ArrayList<News>()
+    //parse data using Klaxon
+    fun extractFeatureFromJson(jsonResponse: String?): ArrayList<News> {
 
-        /*
-        Try to parse the SAMPLE_JSON_RESPONSE. If there's a problem with the way the JSON
-        is formatted, a JSONException exception object will be thrown.
-        Catch the exception so the app doesn't crash, and handle exception.
-        */
-        try {
-            // TODO: Parse the response given by the jsonResponse string
+        var arrayListOfNews = ArrayList<News>()
+        JsonReader(StringReader(jsonResponse)).use { reader ->
+            reader.beginObject() {
 
-            val news_json_response = JSONObject(jsonResponse)
-            if (news_json_response.has("response")) {
-                val response = news_json_response.getJSONObject("response")
-                if (response.has("results")) {
-                    val resultsArray = response.getJSONArray("results")
+                while (reader.hasNext()) {
 
-
-                    for (i in 0 until resultsArray.length()) {
-                        var webTitle: String = "No title"
-                        var newsWebUrl: String = "No name"
-                        var authorTitle: String = "No author"
-
-                        val resultDetails = resultsArray.getJSONObject(i)
-                        if (resultDetails.has("webTitle")) {
-                            webTitle = resultDetails.getString("webTitle")
-                        }
-                        if (resultDetails.has("webUrl")) {
-                            newsWebUrl = resultDetails.getString("webUrl")
-                        }
-                        if (resultDetails.has("tags")) {
-                            val tagsArray = resultDetails.getJSONArray("tags")
-                            if (tagsArray.length() > 0) {
-                                val tagsDetails = tagsArray.getJSONObject(0)
-                                if (tagsDetails.has("webTitle")) {
-                                    authorTitle = tagsDetails.getString("webTitle")
-                                }
-                            }
-                        }
-                        news.add(News(webTitle, authorTitle, newsWebUrl))
-                    }
+                    /***** Issue at the line below */
+                    val newsItem = Klaxon()
+                            .parse<News>(reader)
+                    Log.v("my_tag", "title is: " + newsItem?.title)
+                    arrayListOfNews.add(newsItem!!)
                 }
-            }
-        } catch (e: JSONException) {
-            //handle exception
-        }
 
-        // Return the list of news
-        return news
+            }
+        }
+        return arrayListOfNews
+
     }
+
+
+//    /**
+//     * Return a list of [News] objects that has been built up from
+//     * parsing a JSON response.
+//     */
+//   fun extractFeatureFromJson(jsonResponse: String?): ArrayList<News> {
+//
+//        // Create an empty ArrayList that we can start adding news to
+//        val news = ArrayList<News>()
+//
+//        /*
+//        Try to parse the SAMPLE_JSON_RESPONSE. If there's a problem with the way the JSON
+//        is formatted, a JSONException exception object will be thrown.
+//        Catch the exception so the app doesn't crash, and handle exception.
+//        */
+//        try {
+//            // TODO: Parse the response given by the jsonResponse string
+//
+//            val newsJsonResponse = JSONObject(jsonResponse)
+//            if (newsJsonResponse.has("response")) {
+//                val response = newsJsonResponse.getJSONObject("response")
+//                if (response.has("results")) {
+//                    val resultsArray = response.getJSONArray("results")
+//
+//
+//                    for (i in 0 until resultsArray.length()) {
+//                        var webTitle: String = "No title"
+//                        var newsWebUrl: String = "No name"
+//                        var authorTitle: String = "No author"
+//
+//                        val resultDetails = resultsArray.getJSONObject(i)
+//                        if (resultDetails.has("webTitle")) {
+//                            webTitle = resultDetails.getString("webTitle")
+//                        }
+//                        if (resultDetails.has("webUrl")) {
+//                            newsWebUrl = resultDetails.getString("webUrl")
+//                        }
+//                        if (resultDetails.has("tags")) {
+//                            val tagsArray = resultDetails.getJSONArray("tags")
+//                            if (tagsArray.length() > 0) {
+//                                val tagsDetails = tagsArray.getJSONObject(0)
+//                                if (tagsDetails.has("webTitle")) {
+//                                    authorTitle = tagsDetails.getString("webTitle")
+//                                }
+//                            }
+//                        }
+//                        news.add(News(webTitle, authorTitle, newsWebUrl))
+//                    }
+//                }
+//            }
+//        } catch (e: JSONException) {
+//            //handle exception
+//        }
+//
+//        // Return the list of news
+//        return news
+//    }
 }
 
