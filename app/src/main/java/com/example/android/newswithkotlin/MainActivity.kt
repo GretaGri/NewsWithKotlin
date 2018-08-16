57d96920b53adeb00d20303ea0f1fc24b02bcb48
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2018-08-16 23:03:40 +0530

    Checked Internet connectivity before fetching news|EmptyView added

--- app/src/main/java/com/example/android/newswithkotlin/MainActivity.kt
+++ app/src/main/java/com/example/android/newswithkotlin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.android.newswithkotlin
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -8,6 +10,7 @@ import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -19,11 +22,13 @@ class MainActivity : AppCompatActivity() {
 
     // declare recyclerView instance with lateinit so that it can be handled without any NPE
     lateinit var recyclerView: RecyclerView
+    lateinit var emptyView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
 
         fab.setOnClickListener { view ->
             Toast.makeText(this, "Sorry, this feature is not available", Toast.LENGTH_SHORT).show()
@@ -36,19 +41,29 @@ class MainActivity : AppCompatActivity() {
 
         //Make a method call to fetch data from the Guardian api using Retrofit and Gson and to setup RecyclerView alongwith
         recyclerView = findViewById(R.id.recycler_view)
-        fetchDataFromGuardianUsingRetrofit()
+
+        emptyView = findViewById(R.id.empty_view)
+
+        if (internetIsActive()) {
+            fetchDataFromGuardianUsingRetrofit()
+        } else {
+            emptyView.text = getString(R.string.internet_not_connected)
+        }
+    }
+
+    private fun internetIsActive(): Boolean {
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
     }
 
     private fun fetchDataFromGuardianUsingRetrofit() {
+        emptyView.text = getString(R.string.loading_news)
         val apiInterface = APIClient.client.create(APIInterface::class.java)
-
-
         /**
         GET List of news
          **/
-        val call = apiInterface.getNewsList("results")
-
-
+        val call = apiInterface.getNewsList("virat kohli")
         call.enqueue(object : Callback<GsonNewsResponse> {
             override fun onResponse(call: Call<GsonNewsResponse>, response: Response<GsonNewsResponse>?) {
                 val resource = response?.body()
@@ -59,12 +74,16 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<GsonNewsResponse>, t: Throwable?) {
+                emptyView.text = getString(R.string.failed_server_response, t?.message)
                 Log.v("my_tag", "fail message is: " + t?.message)
             }
         })
     }
 
     fun setupRecyclerView(context: Context, listOfNews: ArrayList<News>) {
+        if (listOfNews.isEmpty() || listOfNews.size == 0) {
+            emptyView.text = getString(R.string.empty_response_received)
+        }
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = RecyclerViewAdapter(listOfNews, context)
     }
