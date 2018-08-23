652c662ccbc1b99face2081bb67145efdfe1522c
Greta Gri <greta.klimaite@gmail.com>
2018-08-24 00:16:12 +0300

    Added NewsDao class for Room database.

---
+++ app/src/main/java/com/example/android/newswithkotlin/database/NewsDao.kt
@@ -0,0 +1,35 @@
+package com.example.android.newswithkotlin.database
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.*
+
+/**
+ * Created by Greta Grigutė on 2018-08-23.
+ */
+@Dao
+interface NewsDao {
+
+    //a method to get all the news, which returns list of news.
+    @Query("SELECT * FROM newstable")
+    fun loadAllNews(): LiveData<List<GsonNewsResponse>>
+
+    //a method to insert one news item.
+    @Insert
+    fun insertNews(newsItem: News)
+
+    //
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun updateNews(newsItem: News)
+
+    //a method to delete one news item.
+    @Delete
+    fun deleteNews(newsItem: News)
+
+    //a method to delete all news.
+    @Query("DELETE FROM newstable")
+    abstract fun deleteAll()
+
+    //a method to select news item with id
+    @Query("SELECT * FROM newstable WHERE id = :id")
+    fun loadTaskById(id: Int): LiveData<GsonNewsResponse>
+}
